a0b6c70d1302a38cdddb81ab5f897fa05e154731
CalebCamargoS <caleb.camargo.s@uni.pe>
2025-09-09 13:21:00 -0500

    checkpoint

--- controller.py
+++ controller.py
@@ -5,8 +5,8 @@ class PIDController():
     def __init__(self, robot):
         self.robot = robot
         self.speed = 0.5
-        self.kp = 5
-        self.ki = 0
+        self.kp = 10
+        self.ki = 0.1
         self.kd = 0
         self.previous_error = 0
         self.integral_error = 0
